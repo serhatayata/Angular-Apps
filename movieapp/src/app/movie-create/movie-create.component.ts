147c44a0e6378d8b5843124b1deb32e37c65bba1
serhatayata <srhtayata@gmail.com>
2022-05-05 18:52:33 +0300

    Template-driven form

--- movieapp/src/app/movie-create/movie-create.component.ts
+++ movieapp/src/app/movie-create/movie-create.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Category } from '../models/category';
+import { AlertifyService } from '../services/alertify.service';
 import { CategoryService } from '../services/category.service';
 import { MovieService } from '../services/movie.service';
 
@@ -12,29 +14,71 @@ import { MovieService } from '../services/movie.service';
 })
 export class MovieCreateComponent implements OnInit {
 
-  constructor(private categoryService:CategoryService, private movieService:MovieService, private router:Router) { }
+  constructor(private categoryService:CategoryService, 
+              private movieService:MovieService, 
+              private router:Router,
+              private alertify:AlertifyService
+              ) { }
 
   categories:Category[];
+  error:string;
+  model:any = {};
 
-  createMovie(title:any,description:any,imageUrl:any,categoryId:any){
+  createMovie(form:NgForm){
 
-    const movie = { id:0,
-                    title:title.value, 
-                    description:description.value, 
-                    imageUrl:imageUrl.value, 
-                    datePublished: new Date().getTime(),
-                    categoryId:categoryId.value,
-                    isPopular:false
-                  };
+    console.log(this.model)
+    console.log(form)
+
+
+    // if(title.value === "" || description.value ==="" || imageUrl.value ==="" || categoryId.value==="-1")
+    // {
+    //   this.alertify.error("Inputs cannot be empty...");
+    //   return;
+    // }
+
+    // if(title.value.length < 5)
+    // {
+    //   this.alertify.error("Title cannot have less then 5 characters.");
+    //   return;
+    // }
+
+    // if(description.value.length < 10 || description.value.length > 50)
+    // {
+    //   this.alertify.error("Description can be between 10-50 characters.");
+    //   return;
+    // }
+
+    // const extensions = ["jpeg","jpg","png"];
+    // const extension = imageUrl.value.split('.').pop();
     
-    this.movieService.createMovie(movie).subscribe(data => {
-      console.log(data);
-      this.router.navigate(['/movies', data.id]);
-    });
+    // if(extensions.indexOf(extension) === -1)
+    // {
+    //   this.alertify.error("You can only use 'JPEG' - 'JPG' - 'PNG'");
+    //   return;
+    // }
+
+    // const movie = { id:0,
+    //   title:title.value, 
+    //   description:description.value, 
+    //   imageUrl:imageUrl.value, 
+    //   datePublished: new Date().getTime(),
+    //   categoryId:categoryId.value,
+    //   isPopular:false
+    // };
+
+    // this.movieService.createMovie(movie).subscribe(data => {
+    //   console.log(data);
+    //   this.router.navigate(['/movies', data.id]);
+    // });
 
     
 
   }
+
+  log(value:any){
+    console.log(value);
+  }
+  
 
   ngOnInit(): void {
     this.categoryService.getCategories().subscribe( data => {
